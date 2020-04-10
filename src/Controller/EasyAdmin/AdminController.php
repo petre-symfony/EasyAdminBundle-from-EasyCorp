1249be888f58ffd82a7f8e7fbecc50c94d3873a9
petrero <petrero@gmail.com>
2020-04-10 16:10:18 +0300

    22.2.Dashboard & Menu Customizations{Adding a Dashboard}
    Adding a Dashboard
    If you downloaded the course code, you should have a tutorial/ directory. Inside, it has an AdminController with a dashboardAction(). Copy that. Then, in src/Controller/EasyAdmin, open our AdminController and paste it there.
    Thanks to the prefix on the route import:
    config/routes/easy_admin.yaml
    easy_admin_bundle:
        prefix: /easyadmin
    This creates a new /easyadmin/dashboard route named admin_dashboard. Oh, I'm missing my use statement for @Route. I'll re-type that and hit enter so that it auto-completes the use statement on top.
    Perfect!
    This renders a template, which I will also grab from the tutorial/ directory. Paste that in templates/easy_admin.
    At this point... the page should work. Cool... but how can I tell EasyAdminBundle to show this page when we go to the admin section's homepage? Right now, if you go directly to /easyadmin, it will take you to whatever the first-defined entity section is... so Genus.

--- src/Controller/EasyAdmin/AdminController.php
+++ src/Controller/EasyAdmin/AdminController.php
@@ -3,10 +3,26 @@
 
 namespace App\Controller\EasyAdmin;
 
+use App\Entity\Genus;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController as BaseAdminController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends BaseAdminController {
 	public function exportAction(){
 		throw new \RuntimeException('Action for exporting an entity is not defined');
+	}
+
+	/**
+	 * @Route("/dashboard", name="admin_dashboard")
+	 */
+	public function dashboardAction() {
+		$em = $this->getDoctrine()->getManager();
+		$genusRepository = $em->getRepository(Genus::class);
+
+		return $this->render('easy_admin/dashboard.html.twig', [
+			'genusCount' => $genusRepository->getGenusCount(),
+			'publishedGenusCount' => $genusRepository->getPublishedGenusCount(),
+			'randomGenus' => $genusRepository->findRandomGenus()
+		]);
 	}
 }
--- src/Repository/GenusRepository.php
+++ src/Repository/GenusRepository.php
@@ -42,4 +42,33 @@ class GenusRepository extends ServiceEntityRepository {
       ->andWhere(Criteria::expr()->gt('yearsStudied', 20))
       ->orderBy(['yearsStudied', 'DESC']);
   }
+
+	public function getGenusCount() {
+		return $this->createQueryBuilder('genus')
+			->select('COUNT(genus.id)')
+			->getQuery()
+			->getSingleScalarResult();
+	}
+
+	public function getPublishedGenusCount() {
+		return $this->createQueryBuilder('genus')
+			->select('COUNT(genus.id)')
+			->where('genus.isPublished = :isPublished')
+			->setParameter('isPublished', true)
+			->getQuery()
+			->getSingleScalarResult();
+	}
+
+	/**
+	 * @return Genus
+	 */
+	public function findRandomGenus() {
+		// very dirty way to get a "random" result - don't use in a real project!
+		$results = $this->createQueryBuilder('genus')
+			->setMaxResults(10)
+			->getQuery()
+			->execute();
+
+		return $results[array_rand($results)];
+	}
 }
